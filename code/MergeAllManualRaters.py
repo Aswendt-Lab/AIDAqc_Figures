a70d8d9d9323ca60788e638d676e41842d41c403
arefks <arefks@gmail.com>
2023-11-20 10:46:24 +0100

    adjusted figure
    adjusted figure

--- code/MergeAllManualRaters.py
+++ code/MergeAllManualRaters.py
@@ -10,6 +10,7 @@ import glob
 import pandas as pd
 from PIL import Image
 import numpy as np
+import concurrent.futures
 # Step 1: Define the starting path and file pattern
 start_path =  r"C:\Users\aswen\Desktop\Code\Validation3"
 file_pattern_all_images = os.path.join(start_path,"*","manual_slice_inspection",'*.png')
@@ -27,95 +28,66 @@ def create_dataframe(file_list, column_names):
         validator_name = path_elements[-2] if len(path_elements) >= 2 else None
         data.append([file_path, dataset_name, validator_name])
     return pd.DataFrame(data, columns=column_names)
-
 # Create DataFrames
 column_names = ['Path', 'dataset_name', 'validator_name']
 
 manual_slice_inspection_df = create_dataframe(manual_slice_inspection_image_files, column_names)
 validators_df = create_dataframe(validators_image_files, column_names)
 
+# Assuming you already have manual_slice_inspection_df and validators_df DataFrames
 
-# Iterate over unique datasets
-for dataset_name in unique_datasets:
-    # Filter DataFrames based on the current dataset
+# Get unique dataset names and validators
+unique_datasets = manual_slice_inspection_df['dataset_name'].unique()
+unique_validators =  validators_df['validator_name'].unique()
+
+# Create a result DataFrame
+result_column_names = ['Path'] + list(unique_validators)
+result_df = pd.DataFrame(columns=result_column_names)
+
+# Function to process a dataset
+def process_dataset(dataset_name):
     ma_subset = manual_slice_inspection_df[manual_slice_inspection_df['dataset_name'] == dataset_name]
     va_subset = validators_df[validators_df['dataset_name'] == dataset_name]
     
-    # Iterate over manual_slice_inspection images
+    results = []
+    
     for index, ma_row in ma_subset.iterrows():
         ma_path = ma_row['Path']
         ma_image = Image.open(ma_path)
         ma_image_array = np.array(ma_image)
         
-        # Initialize result row
         result_row = {'Path': ma_path}
         
-        # Iterate over validators
         for _, va_row in va_subset.iterrows():
             va_path = va_row['Path']
             va_image = Image.open(va_path)
             va_image_array = np.array(va_image)
             
-            # Calculate the difference
             diff_image = ma_image_array - va_image_array
-            
-            # Check if the difference is zero
             is_same = np.sum(diff_image) == 0
             
-            # Update the result row
             result_row[va_row['validator_name']] = is_same
-            
-        # Append the result row to the result DataFrame
-        result_df = result_df.append(result_row, ignore_index=True)
+        
+        results.append(result_row)
+    
+    return results
+
+
+# Get the max number of CPUs
+max_cpus = min(os.cpu_count(), len(unique_datasets))
+
+# Create a ThreadPoolExecutor
+with concurrent.futures.ThreadPoolExecutor(max_cpus) as executor:
+    # Process datasets in parallel
+    results_list = list(executor.map(process_dataset, unique_datasets))
+
+# Concatenate the results into the final DataFrame
+result_df = pd.concat([pd.DataFrame(result_list) for result_list in results_list], ignore_index=True)
+
+# Display the result DataFrame
+print(result_df.head())
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-file_pattern_voting_images = 
-# Step 2: Find all matching CSV files in the specified directory and its subdirectories
-image_files = glob.glob(os.path.join(start_path, '*', file_pattern), recursive=True)
-
-# Step 3: Initialize an empty DataFrame to store the extracted data
-combined_df = pd.DataFrame()
-combined_df1 = pd.DataFrame()
-
-
-# Step 4: Loop through the CSV files and extract the specified columns
-for image_file in image_files:
-    try:
-        df = pd.read_csv(image_file)
-        selected_columns = ["Pathes",	"sequence_name",	"corresponding_img",
-                            "sequence_name",	"One_class_SVM",
-                            "IsolationForest",	"LocalOutlierFactor",
-                            " EllipticEnvelope",	"statistical_method",
-                            "Voting outliers (from 5)"]
-        df = df[selected_columns]
-        df["dataset"] = csv_file.split(os.sep)[-2]
-        # Concatenate the current DataFrame with the combined DataFrame
-        combined_df1 = pd.concat([combined_df1, df], ignore_index=True)
-    except Exception as e:
-        print(f"Error reading {csv_file}: {e}")
-
-
-
-combined_df = combined_df1
-# Step 5: Print the combined DataFrame
-print(combined_df)
-
-# Optionally, you can save the combined DataFrame to a CSV file
-p = r"C:\Users\aswen\Desktop\Code\AIDAqc_Figures\input"
-combined_df.to_csv(os.path.join(p,'combined_votings3.csv'), index=False)
